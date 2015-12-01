3e6b3d5747646b01800b45fb744247aeaf45077b
zmwebdev <zmwebdev@gmail.com>
2015-12-01 12:40:03 +0000

    package

--- app.js
+++ app.js
@@ -20,6 +20,6 @@ app.get('/', function (req, res) {
 });
 
 
-var server = app.listen(process.env.PORT || 3000, function(){
+var server = app.listen(process.end.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3000, function(){
     console.log('Listening in port %d', server.address().port);
 });
